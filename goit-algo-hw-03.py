c55b827034a55345359e902f604827b7e3c733ac
eiforial <rabudal743@gmail.com>
2024-03-08 15:12:02 +0100

    com1

---
+++ goit-algo-hw-03.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+def get_days_from_today():
+        try:
+                date = input("Введите дату в формате ГГГГ-ММ-ДД: ")
+                date_today = datetime.today()
+                user_date = datetime.strptime(date, "%Y-%m-%d")
+                difference = date_today - user_date
+        
+                print(f"Разница между датами состовляет {difference.days} дней")
+
+                get_days_from_today()
+
+        except ValueError as e:
+
+            print("Неправильный формат даты. Используйте формат ГГГГ-ММ-ДД.", e)
+
+            get_days_from_today()
+
+get_days_from_today()
