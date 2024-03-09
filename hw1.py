1e3e5d671da9cc5bd35bd6259e4c5dc0262eb00f
eiforial <rabudal743@gmail.com>
2024-03-10 00:20:34 +0100

    hw1.py

--- hw1.py
+++ hw1.py
@@ -1,20 +1,17 @@
 from datetime import datetime
 
-def get_days_from_today():
-        try:
-                date = input("Введите дату в формате ГГГГ-ММ-ДД: ")
-                date_today = datetime.today()
-                user_date = datetime.strptime(date, "%Y-%m-%d")
-                difference = date_today - user_date
-        
-                print(f"Разница между датами состовляет {difference.days} дней")
+date = '2024-04-15'
 
-                get_days_from_today()
+def get_days_from_today(date):
+    try:
+        user_date = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        return "Ошибка: Неправильный формат даты. Используйте формат YYYY-MM-DD."
 
-        except ValueError as e:
+    date_today = datetime.today()
+    delta = date_today - user_date
+    
+    return delta.days
 
-            print("Неправильный формат даты. Используйте формат ГГГГ-ММ-ДД.", e)
-
-            get_days_from_today()
-
-get_days_from_today()
+days_difference = get_days_from_today(date)
+print("Разница в днях:", days_difference)
