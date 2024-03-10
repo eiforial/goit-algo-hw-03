bdd1a91ac68f0f5a3da2858949e26b02e2daa97d
eiforial <rabudal743@gmail.com>
2024-03-10 02:05:45 +0100

    hw2.py

--- hw2.py
+++ hw2.py
@@ -1,33 +1,33 @@
 import random
 
-min = 1
-max = 100
-quantity = 10
+
 
 def get_number_ticket(min, max, quantity):
     
     if type(min) != int or type(max) != int or type(quantity) != int:
-        print("min, max, quantity должны бить целым числом")
+        
         return None
     
     if min > max:
-        print("min должно быть меньше чем max")
+        
         return None
     
     if quantity < 0:
-        print("quantity не может быть отрицательным")
+        
         return None
 
     if quantity > max - min + 1:
-        print("количество выведенных чисел не может быть больше количества заданного диапазоном")
+        
         return None
     
     numbers = set()
+
     while len(numbers) < quantity:
+        
         numbers.add(random.randint(min, max))
     
     return sorted(list(numbers))
 
-ticket_num = get_number_ticket(min, max, quantity)
+ticket_num = get_number_ticket(1, 49, 6)
 
 print(ticket_num)
